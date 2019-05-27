2d4444b85d6ae4bfc4a637a90ec408ade25c89c9
zokMeodoff <svetlanachuma@gmail.com>
2019-05-27 13:46:17 +0300

    Конкретные хендлеры и добавление правил вынесены в отдельный файл

--- README.md
+++ README.md
@@ -16,5 +16,5 @@ $ pip install -r requirements.txt
 ### Запуск
 
 ```консоль
-$ uwsgi --http :9090 --wsgi-file app.py
+$ uwsgi --http :9090 --wsgi-file example.py
 ```
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import re
 from response import Response
 
+
 class App:
     def __init__(self):
         self.urls_handlers = {}
@@ -24,7 +25,6 @@ class App:
         start_response(status_code, headers)
         return [response.content.encode('utf-8')]
 
-
     def add_url(self, url, handler):
         self.urls_handlers[url] = handler
 
@@ -35,25 +35,6 @@ class App:
         return Response(content, status)
 
 
-def index_handler(env):
-    content = generate_view('index.html')
-    return Response(content)
-
-
-def info_handler(env):
-    content = generate_view('info.html')
-    return Response(content)
-
-
-def redirect_handler(env):
-    status = '301'
-    content = ''
-    response = Response(content, status)
-    response.headers['location'] = env['PATH_INFO'] + r'/'
-    print(h for h in response.headers)
-    return response
-
-
 def generate_view(template_name):
     template_path = os.path.join('templates', template_name)
     if not os.path.exists(template_path):
@@ -61,9 +42,3 @@ def generate_view(template_name):
     with open(template_path, 'r') as template_view:
         template = template_view.read()
     return template
-
-
-application = App()
-application.add_url(r'^/$', index_handler)
-application.add_url(r'^/info/$', info_handler)
-application.add_url(r'^.*(?<!/)$', redirect_handler)
